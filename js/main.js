3145702904ba2dab241b268d5bc4e45245348103
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-02 15:40:02 +0100

    il tutto si ripeterà per 3 volte

--- js/main.js
+++ js/main.js
@@ -1,66 +1,70 @@
-let utentePadiDispari = (prompt('PARI o DISPARI ? Fai la tua scelta'));
-console.log(utentePadiDispari);
+for (let i = 0; i < 4; i++) {
 
-let numeroUtente = parseInt(prompt('Inserisci un nuemro da 1 a 5'));
-console.log("Numero Utente " + numeroUtente);
+    let utentePadiDispari = (prompt('PARI o DISPARI ? Fai la tua scelta'));
+    console.log(utentePadiDispari);
 
-
-// numero casuale generato per il computer da 1 a 5
-
-function generaNumeroCasuale(min, max) {
-
-    let numeroCasuale = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    return numeroCasuale;
-}
-
-let numeroPC = generaNumeroCasuale(1, 5);
-console.log("Numero Pc " + numeroPC);
+    let numeroUtente = parseInt(prompt('Inserisci un nuemro da 1 a 5'));
+    console.log("Numero Utente " + numeroUtente);
 
 
-// eseguo la somma dei due risultati
+    // numero casuale generato per il computer da 1 a 5
 
-let risultatoSomma = sommaDueNumeri(numeroUtente, numeroPC);
-console.log("Somma " + risultatoSomma);
+    function generaNumeroCasuale(min, max) {
 
-function sommaDueNumeri(primoNumero, secondoNumero) {
+        let numeroCasuale = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    let somma = primoNumero + secondoNumero;
-
-    return somma;
-}
-
-// calcolo pari e dispari
-
-function calcolaPariDispari(numeroDaControllare) {
-
-    let risultato = "";
-
-    if (numeroDaControllare % 2 == 0) {
-        risultato = 'PARI';
-    } else {
-        risultato = 'DISPARI';
+        return numeroCasuale;
     }
 
-    return risultato;
-
-}
-
-// verifico se la somma è pari o dispari
-
-const checkRisultato = calcolaPariDispari(risultatoSomma);
-console.log("La somma dei due numeri è " + checkRisultato);
+    let numeroPC = generaNumeroCasuale(1, 5);
+    console.log("Numero Pc " + numeroPC);
 
 
-// vincitore
+    // eseguo la somma dei due risultati
 
-if (checkRisultato == utentePadiDispari) {
-    alert("HAI VINTO!");
-    console.log("Vince l'utente");
+    let risultatoSomma = sommaDueNumeri(numeroUtente, numeroPC);
+    console.log("Somma " + risultatoSomma);
 
-} else {
-    alert("MI DISPIACE HAI PERSO");
-    console.log('Vince il pc');
+    function sommaDueNumeri(primoNumero, secondoNumero) {
+
+        let somma = primoNumero + secondoNumero;
+
+        return somma;
+    }
+
+    // calcolo pari e dispari
+
+    function calcolaPariDispari(numeroDaControllare) {
+
+        let risultato = "";
+
+        if (numeroDaControllare % 2 == 0) {
+            risultato = 'PARI';
+        } else {
+            risultato = 'DISPARI';
+        }
+
+        return risultato;
+
+    }
+
+    // verifico se la somma è pari o dispari
+
+    const checkRisultato = calcolaPariDispari(risultatoSomma);
+    console.log("La somma dei due numeri è " + checkRisultato);
+
+
+    // vincitore
+
+    if (checkRisultato == utentePadiDispari) {
+        alert("HAI VINTO!");
+        console.log("Vince l'utente");
+
+    } else {
+        alert("MI DISPIACE HAI PERSO");
+        console.log('Vince il pc');
+    }
+
 }
 
 alert("VEDI CONSOLE PER DETTAGLIO...");
